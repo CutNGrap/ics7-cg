c9b073822f548789202e88520acfe6c9a8d91610
Alexey <grapess2003@mail.ru>
2022-04-29 23:20:10 +0300

    lab_06

--- lab_06/main.py
+++ lab_06/main.py
@@ -13,8 +13,27 @@ DEL_COUNT = 1
 
 
 class myScene(QtWidgets.QGraphicsScene):
+    def __init__(self, a, b, c, d):
+        super().__init__(a, b, c, d)
+        self.flag = 0
+
+
     def mousePressEvent(self, e):
-        add_point(round(e.scenePos().x()), round(e.scenePos().y()))
+        global win
+        x, y = round(e.scenePos().x()), round(e.scenePos().y())
+        if win.rad_edge.isChecked():
+            self.flag = 1
+            add_point(x, y)
+        else:
+            win.seed = [x,y]
+            win.fill_seed()
+
+    def mouseReleaseEvent(self, e):
+        self.flag = 0
+
+    def mouseMoveEvent(self, e):
+        if self.flag == 1:
+            self.mousePressEvent(e)
 
     def keyPressEvent(self, e):
         global win
@@ -54,8 +73,6 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         self.color_edge = self.t_color_edge
         self.pen = QPen(self.color_edge)
         
-        self.dots = []
-        self.edges = []
         self.close_dot = None
         self.color_ind_fill.setStyleSheet("background-color:" + self.color_fill.name() + ";border: 1px solid black;")
         self.color_ind_bg.setStyleSheet("background-color:" + self.color_bg.name() + ";border: 1px solid black;")
@@ -67,38 +84,52 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         self.msg = QMessageBox()
         self.msg.setIcon(QMessageBox.Critical)
         self.msg.setWindowTitle("Ошибка ввода")
-        self.listWidget.addItem("{:<6s} {:^6s}".format("X", "Y"))
-
+        self.rad_edge.setChecked(True)
+    
+        self.last_x = None
+        self.last_y = None
+        self.close_dot = None
 
         self.horiz = False
         self.vert = False
         self.cur_len = 0
 
-        self.image = QImage()
+        self.image = QImage(self.rcontent.width(), self.rcontent.height(), QImage.Format_ARGB32_Premultiplied)
         self.pix = QPixmap()
 
         self.buttons = [self.but_clear,self.but_close,self.but_color_bg,self.but_color_fill,self.but_color_edge,
-                self.but_color_bg_def,self.but_color_fill_def,self.but_color_edge_def,self.but_add_dot,self.but_fill]
+                self.but_color_bg_def,self.but_color_fill_def,self.but_color_edge_def,self.but_add_dot, self.but_close_2]
 
         self.bind()
+        self.clear()
 
+    def clear_img(self):
+        p = QPainter()
+        p.begin(self.image)
+        p.setPen(QPen(self.color_edge))
+        brush = QBrush(self.color_bg)
+        p.fillRect(0, 0, self.rcontent.width(), self.rcontent.height(), brush)
+        p.end()
+
+        pix = QPixmap()
+        pix.convertFromImage(self.image)
+        self.scene.addPixmap(pix)
+        
 
     def clear(self):
         self.scene.clear()
         self.color_bg = self.t_color_bg
         self.color_edge = self.t_color_edge 
+        self.clear_img()
         self.color_ind_edge.setStyleSheet("background-color:" + self.color_edge.name() + ";border: 1px solid black;")
-        self.listWidget.clear()
-        self.listWidget.addItem("{:<6s} {:^6s}".format("X", "Y"))
         self.close_dot = None
+        self.last_x = None
         self.dots =[]
         self.edges=[]
         self.pen.setColor(self.color_edge)
         self.cur_len = 0
+        self.seed = None
 
-        brush = QBrush(self.color_bg)
-        pen = QPen(self.color_bg)
-        self.scene.addRect(0, 0, self.rcontent.width(), self.rcontent.height(), brush = brush, pen = pen)
         self.graphicsView.setScene(self.scene) 
 
     def button_lock(self):
@@ -126,7 +157,6 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         self.but_color_edge_def.clicked.connect(self.color_edge_def)
 
         self.but_add_dot.clicked.connect(self.init_dot_add)
-        self.but_fill.clicked.connect(self.fill_by_edges)
 
     def init_dot_add(self):
         x,y = self.get_dot_vals()
@@ -160,14 +190,23 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         
 
     def close(self):
-        if self.cur_len < 3:
-            self.msg.setText("Замкнуть фигуру невозможно. Вы ввели менее трех точек для текущей фигуры")
+        if self.close_dot==None:
+            self.msg.setText("Замкнуть фигуру невозможно. Вы еще не ввели ни одной точки!")
             self.msg.show()
-        else:
-            self.scene.addLine(win.dots[-1][0], win.dots[-1][1], self.close_dot[0], self.close_dot[1], win.pen)
-            self.edges.append([win.dots[-1][0], win.dots[-1][1], self.close_dot[0], self.close_dot[1]])
-            self.close_dot = None
-            self.cur_len = 0
+        self.draw_line(self.close_dot[0], self.close_dot[1], self.last_x, self.last_y)
+        self.close_dot = None
+        self.cur_len = 0
+
+    def draw_line(self, a, b, c, d):
+        p = QPainter()
+        p.begin(win.image)
+        p.setPen(QPen(win.color_edge))
+        p.drawLine(a, b, c, d)
+        p.end()
+
+        pix = QPixmap()
+        pix.convertFromImage(win.image)
+        win.scene.addPixmap(pix)
 
     def color_bg_to_fill(self):
         self.color_fill = self.color_bg
@@ -199,97 +238,105 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
     def delay(self):
         QtWidgets.QApplication.processEvents(QEventLoop.AllEvents, 1)
 
-        """t = QTime.currentTime().addUSecs(1)
+        """t = QTime.currentTime().addMSecs(2)
         while QTime.currentTime() < t:
             QCoreApplication.processEvents(QEventLoop.AllEvents, 1)"""
 
-    def fill_by_edges(self):
-        if self.close_dot != None:
-            self.msg.setText("Фигура не замкнута! Отмена закраски.")
-            self.msg.show()
-            return
+    def set_color(self, x, y, color, p):
+        p.setPen(QPen(color))
+        p.drawPoint(x, y)
+
+    def fill_seed(self):
         self.scene.clear()
-        brush = QBrush(self.color_bg)
-        pen = QPen(self.color_bg)
-        self.scene.addRect(0, 0, self.rcontent.width(), self.rcontent.height(), brush = brush, pen = pen)
-        self.graphicsView.setScene(self.scene) 
 
         global DEL_COUNT
         self.button_lock()
-        scene = QGraphicsScene(0, 0, self.rcontent.width(), self.rcontent.height())
-        brush = QBrush(self.color_bg)
-        pen = QPen(self.color_bg)
-        self.scene.addRect(0, 0, self.rcontent.width(), self.rcontent.height(), brush = brush, pen = pen)
-        image = QImage(self.rcontent.width(), self.rcontent.height(), QImage.Format_ARGB32_Premultiplied)
         pix = QPixmap()
         p = QPainter()
+        p.begin(self.image)
 
-        xm = self.find_max_x()
+        stack = [self.seed]
         delay_count = 0
-        for edge in self.edges:
-            p.begin(image)
-            # если горизонтальное ребро - дальше
-            if edge[1] == edge[3]:
-                continue
-            # иначе определяем границы сканирования
-            if edge[1] > edge[3]:
-                edge[1], edge[3] = edge[3], edge[1]
 
-                edge[0], edge[2] = edge[2], edge[0]
+        while stack:
+            # извлекаем очередной затравочный пиксел
+            point = stack.pop()
+            x, y = point[0], point[1]
 
-            y = edge[1]
-            end_y = edge[3]
-            
-            dx = (edge[2] - edge[0]) / (edge[3] - edge[1])
-            start_x = edge[0]
+            # заполняем интервал вправо от затравки (включая затравку)
+            # и запоминаем крайний правый пиксел
+            x_cur = x
+            while self.get_color(x_cur, y) != self.color_edge and x_cur < self.rcontent.width() - 1:
+                self.set_color(x_cur, y, self.color_fill, p)
+                x_cur += 1
+            xr = x_cur - 1
 
-            while y < end_y:
-                # определяем пересечение
-                x = start_x
-                while x < xm:
-                    col = QColor(image.pixel(int(x), int(y)))
-                    if col == self.color_fill:
-                        p.setPen(QPen(self.color_bg))
-                    else:
-                        p.setPen(QPen(self.color_fill))
-                    
-                    p.drawPoint(int(x), int(y))
-                    x += 1
+            # заполняем интервал влево от затравки (не включая затравку)
+            # и запоминаем крайний левый пиксель
+            x_cur = x - 1
+            while self.get_color(x_cur, y) != self.color_edge and x_cur > 0:
+                self.set_color(x_cur, y, self.color_fill, p)
+                x_cur -= 1
+            xl = x_cur + 1
+            # Поиск новых затравочных пикселей в интервале xl <= x <= xr на двух соседних строках y+1, y-1
+            for y_ in [y + 1, y - 1]:
+                x = xl
+                while x <= xr:
+                    # флаг перехождения затравки
+                    flag = 0
+                    # ищем (хоть один или крайний правый) затравочный пиксель
+                    while ((self.get_color(x, y_) != self.color_edge) and
+                           (self.get_color(x, y_) != self.color_fill) and
+                           (x <= xr)) and x < self.rcontent.width() - 1 and y_ > 0 and y_ < self.rcontent.height() - 1:                                                      ###
+                        flag = 1
+                        x += 1
+                    # если такой нашелся, то помещаем его в стек
+                    if flag:
+                        # дошли до конца интервала
+                        if ((self.get_color(x, y_) != self.color_edge) and
+                           (self.get_color(x, y_) != self.color_fill) and
+                           (x == xr)) and x > 0 and y_ > 0 and y_ < self.rcontent.height():
+                            stack.append([x, y_])
+                        # встретили границу или уже заполненную часть на интервале
+                        else:
+                            stack.append([x - 1, y_])
 
-                start_x += dx
-                y += 1
-
-                self.draw_edges(image, p)
-                if self.checkBox.isChecked() and delay_count == DEL_COUNT:
-                    self.redraw_scene() 
-                    pix.convertFromImage(image)
-                    self.scene.addPixmap(pix)
-                    self.delay()
-                    delay_count = 0
-                delay_count+=1
-
-            if not self.checkBox.isChecked():
-                pix.convertFromImage(image)
+                    # Поиск нового интервала в случае прерывания текущего интервала (произойдет, если  x<xr)
+                    # запоминаем абсциссу текущего пиксела
+                    xn = x
+                    # flag = 0
+                    while (((self.get_color(x, y_) == self.color_edge) or
+                           (self.get_color(x, y_) == self.color_fill)) and
+                           (x < xr)) and x < self.rcontent.width() - 1:
+                        x += 1
+                    # убедимся, что координата абсциссы пикселя увеличилась (чтобы не зациклиться)
+                    if x == xn:
+                        x += 1
+            # если выбрана опция "с задержкой",
+            if self.checkBox.isChecked() and delay_count == DEL_COUNT:
+                self.scene.clear()
+                pix.convertFromImage(self.image)
                 self.scene.addPixmap(pix)
-            p.end()
-        p.begin(image)
-        self.draw_edges(image, p)
+                self.delay()
+                delay_count = 0
+            delay_count+=1
+
         p.end()
-        pix.convertFromImage(image)
+        pix.convertFromImage(self.image)
         self.scene.addPixmap(pix)
         self.button_unlock()
 
-    def redraw_scene(self):
-        self.scene.clear()
-        brush = QBrush(self.color_bg)
-        pen = QPen(self.color_bg)
-        self.scene.addRect(0, 0, self.rcontent.width(), self.rcontent.height(), brush = brush, pen = pen)
-        self.graphicsView.setScene(self.scene)
 
-    def draw_edges(self, image, p):
+    def draw_dot(self, x, y):
+        p = QPainter()
+        p.begin(self.image)
         p.setPen(QPen(self.color_edge))
-        for ed in self.edges:
-            p.drawLine(ed[0], ed[1], ed[2], ed[3])
+        p.drawPoint(x, y)
+        p.end()
+
+        pix = QPixmap()
+        pix.convertFromImage(self.image)
+        self.scene.addPixmap(pix)
 
     def find_max_x(self):
         xmax =  self.dots[0][0]
@@ -298,27 +345,29 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
                 xmax = i[0]
         return xmax
 
-        
+    def get_color(self, x, y):
+        return QColor(self.image.pixel(x, y))
 
 def add_point(x, y):
     global win
+    if win.last_x == None:
+        win.last_x = x
+        win.last_y = y
     if win.close_dot == None:
         win.close_dot = [round(x), round(y)]
+        win.last_x = x
+        win.last_y = y
     else:
         if win.horiz and win.vert:
             return
         elif win.horiz and not win.vert:
-            x = win.dots[-1][0]
+            x = win.last_x
         elif not win.horiz and win.vert:
-            y = win.dots[-1][1]
-        win.edges.append([win.dots[-1][0], win.dots[-1][1], x, y])
-        win.scene.addLine(win.dots[-1][0], win.dots[-1][1], x, y, win.pen)
-    win.dots.append([round(x), round(y)])
-    win.cur_len += 1
-    win.listWidget.addItem("{:<6d} {:^6d}".format(x, y))
-
-    
-
+            y = win.last_y
+        win.draw_line(win.last_x, win.last_y, x, y)
+        win.last_x = x
+        win.last_y = y
+        
 
 
 
